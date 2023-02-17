5ca37c5a8e2c5f16393d4bf735eb6f05ce454fae
Buddy <buddy@b2ggames.com>
2023-02-17 18:35:06 +0900

    part3 두번째 강의 중간 커밋

---
+++ src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Post v-for="post of postList" :key="post.filter" :post="post"></Post>
+    </div>
+</template>
+
+<script>
+    import Post from './Post.vue';
+    
+    export default {
+        name: 'isContainer',
+        components: {
+            Post,
+        },
+        props: {
+            postList: [Object],
+        }
+    }
+</script>
+
+<style>
+</style>
